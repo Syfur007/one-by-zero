744e6c258d83764d689e87dc2531813556d4436f
anichu <anis.cse5.bu@gmail.com>
2022-11-15 02:32:28 +0600

    added login and signup page

---
+++ src/pages/Login/Right.js
@@ -0,0 +1,12 @@
+import React from "react";
+import login from "../../images/Login.png";
+
+const Right = () => {
+	return (
+		<div className="flex-1 w-[50%]">
+			<img src={login} alt="" className="w-full" />
+		</div>
+	);
+};
+
+export default Right;
---
+++ src/pages/Signup/Right.js
@@ -0,0 +1,12 @@
+import React from "react";
+import signup from "../../images/Signup.png";
+
+const Right = () => {
+	return (
+		<div className="flex-1 w-[50%]">
+			<img src={signup} alt="" className="w-full" />
+		</div>
+	);
+};
+
+export default Right;
