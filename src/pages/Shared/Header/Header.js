71a4ab7690e40319b18a6631e7af063c3a6e41e7
anichu <anis.cse5.bu@gmail.com>
2022-11-18 11:42:55 +0600

    added navbar

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,177 +1,96 @@
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-	const navigation = [
-		{ name: "Home", to: "/", current: true },
-		{
-			name: "Contribute",
-			to: "/contribute",
-			current: false,
-		},
-		{
-			name: "Login",
-			to: "/login",
-			current: false,
-		},
-	];
+	const menuItems = (
+		<>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
 
-	function classNames(...classes) {
-		return classes.filter(Boolean).join(" ");
-	}
+			<li>
+				<label htmlFor="my-modal-3" className="btn btn-success rounded-lg">
+					Contribute
+				</label>
+			</li>
+		</>
+	);
 	return (
-		<Disclosure as="nav" className="bg-gray-800">
-			{({ open }) => (
-				<>
-					<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-						<div className="relative flex h-16 items-center justify-between">
-							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-								{/* Mobile menu button*/}
-								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-									<span className="sr-only">Open main menu</span>
-									{open ? (
-										<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-									) : (
-										<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-									)}
-								</Disclosure.Button>
-							</div>
-							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-								<div className="flex flex-shrink-0 items-center">
-									<img
-										className="block h-8 w-auto lg:hidden"
-										src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-										alt="Your Company"
-									/>
-									<img
-										className="hidden h-8 w-auto lg:block"
-										src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-										alt="Your Company"
-									/>
-								</div>
-								<div className="hidden sm:ml-6 mx-auto sm:block">
-									<div className="flex space-x-4 ">
-										{navigation.map((item) => (
-											<NavLink
-												key={item.name}
-												to={item.to}
-												className={classNames(
-													item.current
-														? "bg-gray-900 text-white"
-														: "text-gray-300 hover:bg-gray-700 hover:text-white",
-													"px-3 py-2 rounded-md text-sm font-medium"
-												)}
-												aria-current={item.current ? "page" : undefined}
-											>
-												{item.name}
-											</NavLink>
-										))}
-									</div>
-								</div>
-							</div>
-							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-								<button
-									type="button"
-									className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-								>
-									<span className="sr-only">View notifications</span>
-									<BellIcon className="h-6 w-6" aria-hidden="true" />
-								</button>
+		<>
+			<div className="navbar bg-info text-base-100">
+				<div className="navbar-start">
+					<div className="dropdown">
+						<label tabIndex={0} className="btn btn-ghost lg:hidden">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h8m-8 6h16"
+								/>
+							</svg>
+						</label>
+						<ul
+							tabIndex={0}
+							className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+						>
+							{menuItems}
+						</ul>
+					</div>
+					<a className="btn btn-ghost normal-case text-xl">OneBYZero</a>
+				</div>
+				<div className="navbar-center hidden lg:flex">
+					<ul className="menu menu-horizontal p-0">{menuItems}</ul>
+				</div>
+				<div className="navbar-end">
+					<Link to="/login" className="btn">
+						Login
+					</Link>
+				</div>
+			</div>
 
-								{/* Profile dropdown */}
-
-								<Menu as="div" className="relative ml-3">
-									<div>
-										<Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-											<span className="sr-only">Open user menu</span>
-											<img
-												className="h-8 w-8 rounded-full"
-												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-												alt=""
-											/>
-										</Menu.Button>
-									</div>
-									<Transition
-										as={Fragment}
-										enter="transition ease-out duration-100"
-										enterFrom="transform opacity-0 scale-95"
-										enterTo="transform opacity-100 scale-100"
-										leave="transition ease-in duration-75"
-										leaveFrom="transform opacity-100 scale-100"
-										leaveTo="transform opacity-0 scale-95"
-									>
-										<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-											<Menu.Item>
-												{({ active }) => (
-													<a
-														href="#"
-														className={classNames(
-															active ? "bg-gray-100" : "",
-															"block px-4 py-2 text-sm text-gray-700"
-														)}
-													>
-														Your Profile
-													</a>
-												)}
-											</Menu.Item>
-											<Menu.Item>
-												{({ active }) => (
-													<a
-														href="#"
-														className={classNames(
-															active ? "bg-gray-100" : "",
-															"block px-4 py-2 text-sm text-gray-700"
-														)}
-													>
-														Settings
-													</a>
-												)}
-											</Menu.Item>
-											<Menu.Item>
-												{({ active }) => (
-													<a
-														href="#"
-														className={classNames(
-															active ? "bg-gray-100" : "",
-															"block px-4 py-2 text-sm text-gray-700"
-														)}
-													>
-														Sign out
-													</a>
-												)}
-											</Menu.Item>
-										</Menu.Items>
-									</Transition>
-								</Menu>
-							</div>
+			<div className="">
+				<input type="checkbox" id="my-modal-3" className="modal-toggle" />
+				<div className="modal">
+					<div className="modal-box relative">
+						<label
+							htmlFor="my-modal-3"
+							className="btn btn-sm btn-circle absolute right-2 top-2"
+						>
+							✕
+						</label>
+						<div>
+							<h1 className="text-2xl font-semibold">
+								What are you want to Contribute?
+							</h1>
+							<ul className="text-xl my-5">
+								<li className="p-2 my-2 cursor-pointer transition-all hover:bg-[#3503b4] rounded-sm text-white bg-info">
+									<Link className="block w-full" href="#">
+										questions
+									</Link>
+								</li>
+								<li className="p-2 my-2 cursor-pointer transition-all hover:bg-[#3503b4] rounded-sm text-white bg-info">
+									<Link className="block w-full" href="#">
+										books
+									</Link>
+								</li>
+								<li className="p-2 my-2 cursor-pointer transition-all hover:bg-[#3503b4] rounded-sm text-white bg-info">
+									<Link className="block w-full" href="#">
+										slides
+									</Link>
+								</li>
+							</ul>
 						</div>
 					</div>
-
-					<Disclosure.Panel className="sm:hidden">
-						<div className="space-y-1 px-2 pt-2 pb-3">
-							{navigation.map((item) => (
-								<Disclosure.Button
-									key={item.name}
-									as="Link"
-									to={item.to}
-									className={classNames(
-										item.current
-											? "bg-gray-900 text-white"
-											: "text-gray-300 hover:bg-gray-700 hover:text-white",
-										"block px-3 py-2 rounded-md text-base cursor-pointer font-medium"
-									)}
-									aria-current={item.current ? "page" : undefined}
-								>
-									{item.name}
-								</Disclosure.Button>
-							))}
-						</div>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
+				</div>
+			</div>
+		</>
 	);
 };
 
