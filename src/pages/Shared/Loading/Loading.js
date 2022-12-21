76e898783f6681ac6cfece551d33c3729f72f9d2
anichu <anis.cse5.bu@gmail.com>
2022-12-21 20:50:51 +0600

    changed loading design

--- src/pages/Shared/Loading/Loading.js
+++ src/pages/Shared/Loading/Loading.js
@@ -1,10 +1,10 @@
 import React from "react";
 const Loading = () => {
 	return (
-		<div>
+		<div className="my-10 text-center">
 			<div role="status">
 				<svg
-					className="inline mr-2 w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+					className="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
 					viewBox="0 0 100 101"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
