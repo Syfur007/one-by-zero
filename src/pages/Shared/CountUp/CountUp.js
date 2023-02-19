e15aeb19d28353216581962d125c7fe73544a789
anichu <anis.cse5.bu@gmail.com>
2023-02-19 12:43:34 +0600

    count up

--- src/pages/Shared/CountUp/CountUp.js
+++ src/pages/Shared/CountUp/CountUp.js
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 import { secondary } from "../../../constants/colors";
 const CountUpNumber = ({ name, count }) => {
 	return (
-		<CountUp default={0} end={count} suffix="+" duration={2}>
+		<CountUp start={0} end={count} suffix="+" duration={2}>
 			{({ countUpRef, start }) => (
 				<div
 					className={`flex flex-col items-center justify-center w-40 h-20 mx-5 text-white bg-[${secondary}] rounded-md shadow-lg `}
