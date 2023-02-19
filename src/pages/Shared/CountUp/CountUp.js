02d439dd6b5f59931a47967ea170ef9c71c8f4fa
anichu <anis.cse5.bu@gmail.com>
2023-02-19 14:04:59 +0600

    countup component responsiveness

--- src/pages/Shared/CountUp/CountUp.js
+++ src/pages/Shared/CountUp/CountUp.js
@@ -8,13 +8,13 @@ const CountUpNumber = ({ name, value }) => {
 			if (count < value) {
 				setCount((count) => count + 1);
 			}
-		}, 1);
+		}, 10);
 		return () => clearInterval(interval);
 	}, [count, value]);
 
 	return (
 		<div
-			className={`flex flex-col items-center justify-center w-40 h-20 mx-5 text-white bg-[${secondary}] rounded-md shadow-lg `}
+			className={`flex flex-col items-center justify-center w-40 mt-5 h-20 mx-5 text-white bg-[${secondary}] rounded-md shadow-lg `}
 		>
 			<p>{count}+</p>
 			<h2 className="uppercase">{name}</h2>
