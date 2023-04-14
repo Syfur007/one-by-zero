87b902a4594e04a29be925f5defabd807b937f4e
anichu <anis.cse5.bu@gmail.com>
2023-04-14 16:41:00 +0600

    fix course get call to post

--- src/utils/https/courseHttp.js
+++ src/utils/https/courseHttp.js
@@ -2,8 +2,10 @@ import axios from "axios";
 import { default_url } from "./https";
 
 export const getCourses = async (courseData) => {
+	console.log(courseData);
 	try {
-		const { data } = await axios.get(`${default_url}/api/course/`, courseData);
+		const { data } = await axios.post(`${default_url}/api/course/`, courseData);
+		console.log(data);
 		return data;
 	} catch (er) {
 		console.log(er);
