617051895b4b35f19bc778f8f904ea707f7930eb
Ashik Ghosh <ashikghosh763@gmail.com>
2023-05-12 23:44:56 +0600

    first push

--- src/pages/Profile/Profile/Profile.js
+++ src/pages/Profile/Profile/Profile.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Left from "../Left/Left.js";
 import Right from "../Right/Right.js";
+import Test from "./Test.js";
+
 
 const Profile = () => {
 	return (
-		<div className="flex flex-col sm:flex-row">
+	<div>
+			<div className="flex flex-col sm:flex-row">
 			<Left></Left>
 			<Right></Right>
-		</div>
+		    </div>
+			<div>
+				<Test></Test>
+			</div>
+	</div>
 	);
 };
 
---
+++ src/pages/Profile/Profile/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            This is test.
+        </div>
+    );
+};
+
+export default Test;
